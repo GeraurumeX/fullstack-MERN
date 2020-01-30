7159db4f5dd0d25f0a637f836334a50a00ad6d25
gerardo agustin <gerardoagustin@hotmail.com>
2020-01-30 15:07:58 -0300

    Configuración de app en REACT

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+
+import Navigation from "./components/Navigation";
+import NotesList from "./components/NotesList";
+import CreateNote from "./components/CreateNote";
+import CreateUser from "./components/CreateUser";
+
+function App() {
+    return (
+        <Router>
+            <Navigation />
+            <Route path='/' exact component={NotesList}></Route>
+            <Route path='/edit/:id' component={CreateNote}></Route>
+            <Route path='/create' component={CreateNote}></Route>
+            <Route path='/user' component={CreateUser}></Route>
+        </Router>
+    );
+}
+
+export default App;
