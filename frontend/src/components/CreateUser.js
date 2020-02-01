bd63e75629926677374cd2108a4707d05803dac7
gerardo agustin <gerardoagustin@hotmail.com>
2020-02-01 14:46:47 -0300

    HTTP POST Y DELETE, terminado

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -7,15 +7,34 @@ export default class CreateUser extends Component {
         username: ""
     };
     async componentDidMount() {
-        const res = await axios.get("http://localhost:4000/api/users");
-        this.setState({ users: res.data });
+        this.getUsers();
         console.log(this.state.users);
     }
+
+    getUsers = async () => {
+        const res = await axios.get("http://localhost:4000/api/users");
+        this.setState({ users: res.data });
+    };
 
     onChangeUsername = e => {
         this.setState({
             username: e.target.value
         });
+    };
+
+    onSubmit = async e => {
+        e.preventDefault();
+        await axios.post("http://localhost:4000/api/users", {
+            username: this.state.username
+        });
+        // limpia el estado
+        this.setState({ username: "" });
+        this.getUsers();
+    };
+
+    deleteUser = async id => {
+        await axios.delete("http://localhost:4000/api/users/" + id);
+        this.getUsers();
     };
 
     render() {
@@ -24,14 +43,18 @@ export default class CreateUser extends Component {
                 <div className='col-md-4'>
                     <div className='card card-body'>
                         <h3>Create New User</h3>
-                        <form>
+                        <form onSubmit={this.onSubmit}>
                             <div className='form-group'>
                                 <input
                                     type='text'
                                     className='form-control'
+                                    value={this.state.username}
                                     onChange={this.onChangeUsername}
                                 />
                             </div>
+                            <button type='submit' className='btn btn-primary'>
+                                Save
+                            </button>
                         </form>
                     </div>
                 </div>
@@ -41,6 +64,7 @@ export default class CreateUser extends Component {
                             <li
                                 className='list-group-item list-group-item-action'
                                 key={user._id}
+                                onDoubleClick={() => this.deleteUser(user._id)}
                             >
                                 {user.username}
                             </li>
