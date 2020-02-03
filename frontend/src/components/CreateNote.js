c6c101fc4c18ba18b6fc5aed07a355950defdfb0
gerardo agustin <gerardoagustin@hotmail.com>
2020-02-03 08:45:11 -0300

    Componente Notes terminado Metodos HHTP

--- frontend/src/components/CreateNote.js
+++ frontend/src/components/CreateNote.js
@@ -13,12 +13,22 @@ export default class CreateNote extends Component {
     };
     async componentDidMount() {
         const res = await axios.get("http://localhost:4000/api/users/");
-        this.setState({ users: res.data.map(user => user.username) });
+        this.setState({
+            users: res.data.map(user => user.username),
+            userSelected: res.data[0].username
+        });
     }
 
-    onSubmit = e => {
-        console.log(this.state.title, this.state.content);
+    onSubmit = async e => {
         e.preventDefault();
+        const newNote = {
+            title: this.state.title,
+            content: this.state.content,
+            date: this.state.date,
+            author: this.state.userSelected
+        };
+        await axios.post("http://localhost:4000/api/notes", newNote);
+        window.location.href = "/";
     };
 
     onInputChange = e => {
--- frontend/src/components/NotesList.js
+++ frontend/src/components/NotesList.js
@@ -1,7 +1,50 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { format } from "timeago.js";
 
 export default class NotesList extends Component {
+    state = {
+        notes: []
+    };
+    componentDidMount() {
+        this.getNotes();
+    }
+
+    async getNotes() {
+        const res = await axios.get("http://localhost:4000/api/notes");
+        this.setState({ notes: res.data });
+    }
+
+    deleteNote = async id => {
+        await axios.delete("http://localhost:4000/api/notes/" + id);
+        this.getNotes();
+    };
     render() {
-        return <div>NOTES LIST</div>;
+        return (
+            <div className='row'>
+                {this.state.notes.map(note => (
+                    <div className='col-md-4 p-2' key={note._id}>
+                        <div className='card'>
+                            <div className='card-header'>
+                                <h5>{note.title}</h5>
+                            </div>
+                            <div className='card-body'>
+                                <p>{note.content}</p>
+                                <p>{note.author}</p>
+                                <p>{format(note.date)}</p>
+                            </div>
+                            <div className='card-footer'>
+                                <button
+                                    className='btn btn-danger'
+                                    onClick={() => this.deleteNote(note._id)}
+                                >
+                                    Delete
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
     }
 }
